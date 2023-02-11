a76426c53e7fe12c7b8314194f7fc8b14d601b3b
Julia <artamon-julia@ya.ru>
2023-02-11 16:54:42 +0700

    Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿// 66
+int f(int m, int n)
+{
+  if (m == n)
+    return m;
+  return (m+f(m+1, n));
+}
+
+Console.Clear();
+Console.Write("Введите 1 число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2 число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(f(m,n));
